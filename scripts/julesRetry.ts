481f6d08173da61e9e6621040a4f52ef15d497b4
iHildy <ian@jedi.net>
2025-06-28 01:50:09 -0500

    added coolify supported jules retry cron job command

---
+++ scripts/julesRetry.ts
@@ -0,0 +1,12 @@
+import { retryFlaggedJulesTasks } from "../utils/jules";
+
+(async () => {
+    try {
+        const result = await retryFlaggedJulesTasks();
+        console.log("[Jules Retry]", result);
+        process.exit(0);
+    } catch (error) {
+        console.error("[Jules Retry] failed", error);
+        process.exit(1);
+    }
+})();
